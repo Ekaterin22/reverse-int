bc6360400de4ed4231ba088fff4bb5dfa8eaee7e
Ekaterin22 <77670720+Ekaterin22@users.noreply.github.com>
2021-03-10 16:28:51 +0400

    feat: implement reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0) return reverse(n*(-1));
+
+    strNumber = n.toString()
+    strNumberArray = strNumber.split("");
+    strNumberArray.reverse(); 
+    result = strNumberArray.join(""); 
+    return Number(result);
 }
